a5d9aa9ccbd88f030cbf215a9c5084c734495e0f
Henrique <85001190+HENRIQUE912@users.noreply.github.com>
2023-03-28 19:10:22 -0300

    primeiro site com Javascript
    esse e o primero site usando o código javascript. 
    Como JS agora ele tem a função de click no botão e mostrar um pop informando que foi executado a função

---
+++ funcoes.js
@@ -0,0 +1,13 @@
+function clicou1 () {
+    window.alert("Voçê cliente no botão 1")
+}
+
+function clicou2 () {
+    window.alert("Voçê cliente no botão 2")
+}
+
+function clicou3 () {
+    window.alert("Voçê cliente no botão 3")
+}
+
+
