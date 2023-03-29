3f0827e3cb8eb32056fdb5044eb0ab8b92f687c6
Henrique <85001190+HENRIQUE912@users.noreply.github.com>
2023-03-28 21:37:14 -0300

    Calculador com erro de soma.
    Não faz o calculo, ele envia os numero coletado em sequencia

--- funcoes.js
+++ funcoes.js
@@ -6,8 +6,21 @@ function clicou2 () {
     window.alert("Voçê cliente no botão 2")
 }
 
-function clicou3 () {
+function clicou3 () {0
     window.alert("Voçê cliente no botão 3")
 }
+
+function calc (){
+    let n1 = window.prompt("Digite numero 1")
+    let n2 = window.prompt("Digite numero 2")
+    
+   
+    let result= n1 + n2
+   
+    let res = window.document.getElementById ("resultado").innerHTML = result // Res é a variavael que vai retora para o HTML com o resultado
+   res.innerHTML= "<p>O Resultado é  ${result}  </p>;"
+
+}
+
 
 
