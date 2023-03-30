55bca19bf66f25363ca8a549b490e95e95e2a11c
Henrique <85001190+HENRIQUE912@users.noreply.github.com>
2023-03-30 17:43:26 -0300

    V 2 Agora soma multiplica e dividi
    calculadora agora soma, subtraí, multiplica divido.

--- funcoes.js
+++ funcoes.js
@@ -10,17 +10,60 @@ function clicou3 () {0
     window.alert("Voçê cliente no botão 3")
 }
 
-function calc (){
-    let n1 = window.prompt("Digite numero 1")
-    let n2 = window.prompt("Digite numero 2")
+
+
+
+function ads (){
+    var n1 = window.prompt("Digite numero 1")
+    var n2 = window.prompt("Digite numero 2")
     
+
    
-    let result= n1 + n2
+    let result= parseFloat(n1) + parseFloat(n2)
+    console.log(result)
    
-    let res = window.document.getElementById ("resultado").innerHTML = result // Res é a variavael que vai retora para o HTML com o resultado
-   res.innerHTML= "<p>O Resultado é  ${result}  </p>;"
+    let res = window.document.getElementById ("resultado").innerHTML = result// Res é a variavael que vai retora para o HTML com o resultado
+   //res.innerHTML= "<p>O Resultado é  ${result}  </p>;"
 
 }
 
+function sub (){
+    var n1 = window.prompt("Digite numero 1")
+    var n2 = window.prompt("Digite numero 2")
+    
+
+   
+    let result= n1 - n2 
+   
+    let res = window.document.getElementById ("resultado").innerHTML = result// Res é a variavael que vai retora para o HTML com o resultado
+   //res.innerHTML= "<p>O Resultado é  ${result}  </p>;"
+
+}
+function mult (){
+    var n1 = window.prompt("Digite numero 1")
+    var n2 = window.prompt("Digite numero 2")
+    
+
+   
+  
+
+    let result= n1 * n2 
+   
+    let res = window.document.getElementById ("resultado").innerHTML = result// Res é a variavael que vai retora para o HTML com o resultado
+   //res.innerHTML= "<p>O Resultado é  ${result}  </p>;"
+
+}
+function div (){
+    var n1 = window.prompt("Digite numero 1")
+    var n2 = window.prompt("Digite numero 2")
+    
+
+   
+    let result= n1 / n2 
+   
+    let res = window.document.getElementById ("resultado").innerHTML = result// Res é a variavael que vai retora para o HTML com o resultado
+   res.innerHTML= "<p>O Resultado é  ${result}  </p>;"
+
+}
 
 
